32bae9cd8e62b1dd21df184664d447cf6565acf1
MobileSuitTech <16403672+MobileSuitTech@users.noreply.github.com>
2018-08-30 15:30:36 -0400

    finished the UI

--- Frontend_Files/app.js
+++ Frontend_Files/app.js
@@ -1,15 +1,23 @@
 // Write your JS in here
-
+//alert("HELLO FROM APP.JS!")
 var pics = [
-	"imgs/kitty_bed.jpg",
-	"imgs/kitty_basket.jpg", 
+	"imgs/kitty_bed.JPG",
+	"imgs/kitty_basket.JPG", 
 	"imgs/kitty_laptop.jpg",
 	"imgs/kitty_door.jpg",
-	"imgs/kitty_sink.jpg",
+	"imgs/kitty_sink.JPG",
 	"imgs/kitty_wall.jpg"
 ]
 
+// for doing something with all button : document.querySelectorAll("button")
+var btn = document.querySelector("button")
+var img = document.querySelector("img");
+var counter = 1;
 
-
-//crud-web-demo-app
-// demo app that showcases aptitude in making an interactive web app with database, containerized
+btn.addEventListener("click", function(){
+	//alert("CLICKED")
+	//alert(img.src);
+	if(counter === 6){counter = 0;} 
+	img.src = pics[counter];
+	counter++;
+});
